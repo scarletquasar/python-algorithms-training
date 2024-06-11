feda3642771ac1f7b69a95474f82284e1b5ef8af
Raven <70824102+somecodingwitch@users.noreply.github.com>
2024-06-10 21:41:38 -0300

    Initial commit

---
+++ list-comprehension.py
@@ -0,0 +1,17 @@
+list = [1, 2, 3, 4, 5, 6]
+list_odds = [item for item in list if item % 2 == 0]
+
+print(list_odds)
+
+zeros = [0 for _ in list]
+
+print(zeros)
+
+# This language does have easy intersection, awesome
+pairs = [
+    (x, y)
+    for x in range(2)
+    for y in range(2)
+]
+
+print(pairs)
